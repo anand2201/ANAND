e558820a32372136de719c1caeff5fc1539eddb2
anand2201 <ppanand74@gmail.com>
2017-01-13 12:20:31 +0530

    Add files via upload

---
+++ Rneg.r
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(reshape)
+
+#read data
+data = read.table(file.choose(), header=T,sep=",")
+
+#melt data “rating vs. all”
+data2=melt(data,id=c("rating"))
+data2
+max(data2$rating)
+qplot(rating,value,data=data2,geom="line",linetype=variable)
